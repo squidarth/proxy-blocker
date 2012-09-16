ca11af984c76f108b902ebbf7308e07fd7cdce1f
Sidharth Shanker <sshanker220@gmail.com>
2012-09-15 19:35:36 -0700

    Progress on getting config file working

--- lib/proxy_blocker/config.rb
+++ lib/proxy_blocker/config.rb
@@ -2,7 +2,39 @@ module ProxyBlocker
 
   module Config
     extend self
-  
-  end
 
+    {
+      block_behavior: ["Proc", "Lambda"],
+      redis: ["Redis"]
+    }.each do |k,v|
+      define_method "#{k}=" do |arg|
+        raise ArgumentError unless v.include?(arg.class)
+        instance_variable_set("@#{k}", arg)
+      end
+      
+      define_method k do 
+        self.instance_variable_get("@#{k}")
+      end 
+    end
+
+    def aggregate_method=(meth)
+      raise ArgumentError unless ["Proc", "Lambda"].include?(meth.class)
+      list = meth.call 
+      raise ArgumentError unless list.class = "Array" 
+      set_ip_list(list) 
+    end
+  
+  
+    def set_ip_list(list)
+      if self.instance_variable_get("@redis")
+        write_list_to_redis(list)
+      else
+       instance_variable_set("@ip_list", list)
+      end
+    end 
+  
+    def write_list_to_redis(list)
+      self.redis.write(list)
+    end 
+  end
 end
